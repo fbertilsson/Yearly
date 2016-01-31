3779aa5f1a10fefe6a5ccbef847a9c59863b24f7
Fredrik Bertilsson <fredrik.bertilsson@acando.com>
2016-01-31 21:29:02 +0100

    UI improved - only one grid - added example data

--- Yearly/MainWindow.xaml.cs
+++ Yearly/MainWindow.xaml.cs
@@ -32,6 +32,14 @@ namespace Yearly
         private void OnWindowLoaded(object sender, RoutedEventArgs e)
         {
             tbSource.Focus();
+            tbSource.Text = @"2014-08-01 00:00	2
+2014-09-26 00:07	3
+2015-03-31 23:59	5
+2015-06-01 00:00	2
+2016-01-26 00:07	3
+2016-10-31 23:59	5
+";
+            tbSource.SelectAll();
         }
     }
 }
