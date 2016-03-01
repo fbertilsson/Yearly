b60fae6560bfb842f15ac05168f0ce36645c9dcf
Fredrik Bertilsson <fredrik.bertilsson@acando.com>
2016-03-01 22:16:03 +0100

    Made MVC for register entry look ok, but the controller does not register anything yet.

---
+++ YearlyWeb2/Controllers/RegisterEntryController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YearlyWeb2.Models;
+
+namespace YearlyWeb2.Controllers
+{
+    public class RegisterEntryController : Controller
+    {
+        // GET: RegisterEntry
+        public ActionResult RegisterEntryView()
+        {
+            ViewBag.Title = "Registrera mätarställning";
+            ViewBag.SubTitle = "Registrera aktuell mätarställning";
+            ViewBag.Action = "Registrera";
+
+            var model = new RegisterEntryModel
+            {
+                DateString = DateTime.Now.ToShortDateString(),
+                RegisterValue = string.Empty,
+            };
+            
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult SubmitRegisterEntry(RegisterEntryModel model)
+        {
+            ViewBag.Title = "Mätarställning registrerad";
+            ViewBag.SubTitle = "Mätarställningen blev registrerad";
+            ViewBag.Action = "FOO";
+            return View(model);
+        }
+    }
+}
---
+++ YearlyWeb2/Models/RegisterEntryModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Web.Mvc.Html;
+using System.Web.UI.WebControls;
+
+namespace YearlyWeb2.Models
+{
+    public class RegisterEntryModel
+    {
+        [DisplayName("Datum")]
+        public string DateString { get; set; }
+        
+        /// <summary>
+        /// Mätarställning
+        /// </summary>
+        [DisplayName("Mätarställning")]
+        public string RegisterValue { get; set; }
+    }
+}
