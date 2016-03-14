04b0b90408c8381876e965972d64593874586c45
Fredrik Bertilsson <fredrik.bertilsson@acando.com>
2016-03-14 16:15:30 +0100

    Preparing for using the entered registry value. - Downgraded used .NET version to 4.5 which is supported in Web Role

--- YearlyWeb2/Controllers/RegisterEntryController.cs
+++ YearlyWeb2/Controllers/RegisterEntryController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using Periodic;
 using YearlyWeb2.Models;
 
 namespace YearlyWeb2.Controllers
@@ -28,6 +32,15 @@ namespace YearlyWeb2.Controllers
         [HttpPost]
         public ActionResult SubmitRegisterEntry(RegisterEntryModel model)
         {
+            bool ok;
+            DateTime t;
+            int v;
+            ok = DateTime.TryParse(model.DateString, out t); // TODO error handling
+            ok = int.TryParse(model.RegisterValue, out v); // TODO error handling
+            // TODO FB if (! ok) return ...
+            var tvq = new Tvq(t, v, Quality.Ok);
+
+
             ViewBag.Title = "Mätarställning registrerad";
             ViewBag.SubTitle = "Mätarställningen blev registrerad";
             return View(model);
